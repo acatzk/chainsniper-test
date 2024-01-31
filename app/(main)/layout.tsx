777ac9c2f39a69ac233c39432d2d046460dd5c56
acatzk <joshuaimalay@gmail.com>
2024-01-31 20:02:45 +0800

    refactor: fix scrollable in layout

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -11,9 +11,11 @@ export default function MainLayout({ children }: MainLayoutProps): JSX.Element {
   return (
     <div className="flex h-full overflow-y-auto bg-background text-secondary">
       <Sidebar />
-      <main className="custom-scrollbar mx-auto max-w-6xl flex-1 overflow-y-auto p-6">
-        <Header />
-        {children}
+      <main className="custom-scrollbar flex-1 overflow-y-auto">
+        <div className="mx-auto max-w-6xl p-6">
+          <Header />
+          {children}
+        </div>
       </main>
     </div>
   )
