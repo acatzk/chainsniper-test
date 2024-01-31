7ecc5c18bfbdd20f4064ca8d25898e4ee111d690
acatzk <joshuaimalay@gmail.com>
2024-01-31 16:15:37 +0800

    refactor: update home header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -10,7 +10,9 @@ export default function MainLayout({ children }: MainLayoutProps): JSX.Element {
   return (
     <div className="bg-background text-secondary flex h-full overflow-y-auto">
       <Sidebar />
-      <main className="custom-scrollbar flex-1 overflow-y-auto">{children}</main>
+      <main className="custom-scrollbar mx-auto max-w-6xl flex-1 overflow-y-auto p-6">
+        {children}
+      </main>
     </div>
   )
 }
