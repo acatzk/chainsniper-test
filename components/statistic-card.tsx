a09bb8ceaf970dd90b115057538321805abcb4b1
acatzk <joshuaimalay@gmail.com>
2024-02-01 12:58:12 +0800

    improvement: add abstract dots in cards

--- components/statistic-card.tsx
+++ components/statistic-card.tsx
@@ -3,6 +3,8 @@ import { TrendingDown, TrendingUp } from 'lucide-react'
 import { cn } from '~/lib/utils'
 import { CardStatistic } from '~/constant/statistic-data'
 
+import { AbstractDots } from './custom-icons/abstract-dots'
+
 type StatisticCardProps = {
   stat: CardStatistic
   onClick: () => void
@@ -14,17 +16,22 @@ export const StatisticCard = ({ stat, onClick }: StatisticCardProps): JSX.Elemen
       role="button"
       onClick={onClick}
       className={cn(
-        'w-full rounded-2xl px-7 py-7',
+        'relative w-full overflow-hidden rounded-2xl px-7 py-7',
         stat.isActive ? 'bg-[#0452ef] text-white' : 'cursor-pointer bg-[#121732] text-secondary'
       )}
     >
-      <header className="flex flex-wrap items-center gap-x-3">
+      {stat.isActive && (
+        <div className="absolute -top-14 right-0">
+          <AbstractDots />
+        </div>
+      )}
+      <header className="relative flex flex-wrap items-center gap-x-3">
         <div className={cn('rounded-full p-2', stat.isActive ? 'bg-white' : 'bg-[#383c52]')}>
           <stat.icon className={cn('h-4 w-4', stat.isActive ? 'text-[#060B27]' : 'text-white')} />
         </div>
         <h4 className="text-base">{stat.title}</h4>
       </header>
-      <main className="mt-4">
+      <main className="relative mt-4">
         <h2 className="text-2xl font-semibold">{stat.earn}</h2>
         <div className="mt-2 flex flex-wrap items-center gap-x-2 text-xs">
           <div className="flex flex-wrap items-center space-x-1">
